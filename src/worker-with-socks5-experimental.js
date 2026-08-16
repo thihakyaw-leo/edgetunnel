48a261e9e0f69e9b4ac8fb2c4944b1fbbae6f660
Thiha Kyaw <thihakyaw.dtr@gmail.com>
2026-08-17 03:36:47 +0630

    Update userID to a new UUID

--- src/worker-with-socks5-experimental.js
+++ src/worker-with-socks5-experimental.js
@@ -4,7 +4,7 @@ import { connect } from 'cloudflare:sockets';
 
 // How to generate your own UUID:
 // [Windows] Press "Win + R", input cmd and run:  Powershell -NoExit -Command "[guid]::NewGuid()"
-let userID = 'd342d11e-d424-4583-b36e-524ab1f0afa4';
+let userID = 'a3387ac4-6aa6-466c-ba1c-a881977566e6';
 
 let proxyIP = '';
 
